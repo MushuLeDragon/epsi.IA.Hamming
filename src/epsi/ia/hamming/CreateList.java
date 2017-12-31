78c3343e175ba41f31586e77cff1d90427a534b3
MushuLeDragon <dragon.mushu@hotmail.com>
2017-12-31 13:54:00 +0400

    Add code of Hamming Distance Array

--- src/epsi/ia/hamming/CreateList.java
+++ src/epsi/ia/hamming/CreateList.java
@@ -97,7 +97,43 @@ public class CreateList {
         for (int i = 0; i < numberOfExamples; i++) {
             System.out.println(" | " + TableList[i][0] + " | " + TableList[i][1] + " | " + TableList[i][2] + " | " + TableList[i][3] + " | ");
         }
-//Test commit
+
+        //Java ne supporte pas directement les tableaux à plusieurs dimensions : il faut déclarer un tableau de tableau.
+        int hammingArray[][] = new int[numberOfExamples][numberOfExamples];
+        /*
+        for (int i = 0; i < numberOfExamples; i++) {
+            hammingArray[i] = new int[numberOfExamples];
+        }
+         */
+
+        //Nombre d'exemples étudiés
+        for (int i = 0; i < numberOfExamples; i++) {
+
+            //Nombres de fois ou on compare l'exemple
+            for (int j = (i + 1); j < numberOfExamples; j++) {
+
+                int hammingCount = 0;
+
+                //Nombre de fois où on compare les entités
+                for (int k = 0; k < numberOfEntities; k++) {
+
+                    if (TableList[i][k] != TableList[j][k]) {
+                        hammingCount++;
+                    }
+
+                }
+
+                hammingArray[i][j] = hammingCount;
+                System.out.println(" Nombre de différences entre l'exemple " + (i + 1) + " et l'exemple " + (j + 1) + " : " + hammingArray[i][j]);
+
+            }
+
+        }
+
+        for (int i = 0; i < numberOfExamples; i++) {
+            System.out.println(" | " + hammingArray[i][0] + " | " + hammingArray[i][1] + " | " + hammingArray[i][2] + " | " + hammingArray[i][3] + " | " + hammingArray[i][4] + " | " + hammingArray[i][5] + " | " + hammingArray[i][6] + " | " + hammingArray[i][7] + " | " + hammingArray[i][8] + " | " + hammingArray[i][9] + " | ");
+        }
+
     }
 
 }
